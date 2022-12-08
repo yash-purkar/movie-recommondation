4f95bf9a80ca106384c5f6f038b900f342b7fa32
yashpurkar <yashpurkar7079@gmail.com>
2022-12-09 00:05:23 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./Styles.css";
+import Data from "./Data";
+import { useState } from "react";
 
-function App() {
+export default function App() {
+  const [type, setType] = useState("History");
+  //
+
+  function handleClick(typ) {
+    setType(typ);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Good Movies</h1>
+      <p>
+        Here All of these my favourite movies you can watch to aquire some
+        kowledge about given things.
+      </p>
+
+      {Object.keys(Data).map((typ) => {
+        return <button onClick={() => handleClick(typ)}>{typ}</button>;
+      })}
+
+      {Data[type].map((val) => {
+        return (
+          <ul>
+            <li>{val.heading}</li>
+            <li>{val.rating}</li>
+          </ul>
+        );
+      })}
+
+      <h5 style={{ marginTop: "120px", color: "gray" }}>worikin on style</h5>
     </div>
   );
 }
 
-export default App;
+
---
+++ src/Data.js
@@ -0,0 +1,49 @@
+const data = {
+  History: [
+    {
+      heading: "Black Friday",
+      rating: "8.4/10"
+    },
+    {
+      heading: "Lagaan",
+      rating: "8.1/10"
+    }
+  ],
+  StockMarket: [
+    {
+      heading: "Wall Street",
+      rating: "7.3/10"
+    },
+    {
+      heading: "Trading Places",
+      rating: "7.5/10"
+    }
+  ],
+
+  Cricket: [
+    {
+      heading: "The Little Master",
+      rating: "7.3/10"
+    },
+    {
+      heading: "M.S. Dhoni: The Untold Story",
+      rating: "7.9/10"
+    }
+  ]
+
+  // business
+  //
+
+  //   {
+  //   type : "Stock Market",
+  //   heading : "Margin Call",
+  //   rating : "7.1/10"
+  // },
+  // {
+  //   type: "Historical",
+  //   heading : "Lagaan",
+  //   rating: "8.1/10"
+  // },
+};
+
+export default data;
