8903033d157cfaab473e51809d9d07b0c679d823
yashpurkar <yashpurkar7079@gmail.com>
2022-12-13 08:19:20 +0530

    updated movies

--- src/Data.js
+++ src/Data.js
@@ -1,49 +1,82 @@
 const data = {
   History: [
     {
-      heading: "Black Friday",
+      heading: "Sher Shivraj",
       rating: "8.4/10"
     },
     {
-      heading: "Lagaan",
+      heading: "Har Har Mahadev",
       rating: "8.1/10"
+    },
+    {
+      heading: "Tanhaji",
+      rating: "7.5/10"
+    },
+    {
+      heading: "Farzand",
+      rating: "8.4"
     }
   ],
   StockMarket: [
     {
       heading: "Wall Street",
-      rating: "7.3/10"
+      rating: "7.5/10"
     },
     {
       heading: "Trading Places",
-      rating: "7.5/10"
-    }
+      rating: "7.3/10"
+    },
+    {
+      heading: "Margin Call",
+      rating: "7.1/10"
+    },
+    {
+      heading: "Big Bull",
+      rating: "7.0/10"
+    },
+
   ],
 
   Cricket: [
     {
-      heading: "The Little Master",
-      rating: "7.3/10"
+      heading: "Jersy",
+      rating: "8.5/10"
     },
     {
       heading: "M.S. Dhoni: The Untold Story",
       rating: "7.9/10"
+    },
+
+    {
+      heading: "83",
+      rating: "7.5/10"
+    },
+    {
+      heading: "The Little Master",
+      rating: "7.3/10"
+    }
+  ],
+
+  Motivational: [
+    {
+      heading: "Bhag Milkha Bhag",
+      rating: "8.2/10"
+    }, {
+      heading: "Chakde India",
+    },
+    {
+      heading: "Gully Boy",
+      rating: "7.9/10"
+    },
+    {
+      heading: "Mary Kom",
+      rating: "6.8/10"
+    },
+    {
+      heading: "Lagaan",
+      rating: "8.1/10"
     }
   ]
-
-  // business
-  //
-
-  //   {
-  //   type : "Stock Market",
-  //   heading : "Margin Call",
-  //   rating : "7.1/10"
-  // },
-  // {
-  //   type: "Historical",
-  //   heading : "Lagaan",
-  //   rating: "8.1/10"
-  // },
 };
 
 export default data;
